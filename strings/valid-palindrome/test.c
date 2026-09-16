a3e9b7efff8b62109f611ab1a25408aa302b9df1
tameemmohammed615-hub <tameemmohammed615@gmail.com>
2026-09-16 18:53:37 +0530

    Add valid palindrome  test  case
    Implement a test for validating palindromes ignoring non-alphanumeric characters.

---
+++ strings/valid-palindrome/test.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main()
+{
+    char s[] = "A man, a plan, a canal: Panama";
+    int left = 0;
+    int right = strlen(s) - 1;
+    int valid = 1;
+
+    while (left < right)
+    {
+        while (left < right && !isalnum(s[left]))
+            left++;
+
+        while (left < right && !isalnum(s[right]))
+            right--;
+
+        if (tolower(s[left]) != tolower(s[right]))
+        {
+            valid = 0;
+            break;
+        }
+
+        left++;
+        right--;
+    }
+
+    if (valid)
+        printf("Test Passed\n");
+    else
+        printf("Test Failed\n");
+
+    return 0;
+}
